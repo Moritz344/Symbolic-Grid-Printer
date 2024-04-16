7247081e0e05d3ca37ff49c3d3e8597cf62ab8f6
Moritz Maier <moritzmaier34@gmail.com>
2024-04-16 14:48:30 +0200

    Rectangle

---
+++ Rectangle.py
@@ -0,0 +1,14 @@
+rows = int(input("Enter the number of rows: "))
+columns = int(input("Enter the number of columns: "))
+symbol = input("Enter a symbol to use: ")
+# Sample Output
+
+# # #
+# # #
+# # #
+
+for k in range(columns):
+     for j in range(rows):
+          print(symbol,end=' ')
+     print()
+
